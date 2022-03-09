851888c21ed8561b5fa6567314f69753a486d80c
Midah Pasche <midahannep@gmail.com>
2022-03-09 16:10:54 +0100

    HordeString::convertToUtf8 method

--- src/HordeString.php
+++ src/HordeString.php
@@ -1,6 +1,7 @@
 <?php
 namespace Horde\Util;
 
+use Horde_Util;
 use Error;
 use Exception;
 use Horde_Imap_Client_Utf7imap;
@@ -952,5 +953,52 @@ class HordeString
         return (substr($str, 0, 3) == pack('CCC', 239, 187, 191))
             ? substr($str, 3)
             : $str;
+    }
+
+    /**
+     * Try to convert a string to UTF-8.
+     * Returns null if source charset could not be detected or conversion failed
+     *
+     * @param string $string The string to convert
+     * @return string The string in UTF-8
+     * 
+     * @throws Exception If the source encoding can not be detected
+     * @throws Exception If the iconv extension is not installed
+     * @throws Exception If the conversion via iconv fails
+     */
+    public static function convertToUtf8(string $string): ?string
+    {
+        $encodings = [
+            'UTF-8',
+            'ISO-8859-1',
+            'UTF-16',
+            'ISO-8859-2',
+            'ISO-8859-3',
+            'ISO-8859-4',
+            'ISO-8859-5',
+            'ISO-8859-6',
+            'ISO-8859-7',
+            'ISO-8859-8',
+            'ISO-8859-9',
+            'ISO-8859-10',
+            'ISO-8859-13',
+            'ISO-8859-14',
+            'ISO-8859-15',
+            'ISO-8859-16',
+        ];
+        $enc = mb_detect_encoding($string, $encodings);
+        if (!$enc) {
+            throw new Exception('Can not convert string to UTF-8. Could not detect source encoding.');
+        } elseif ($enc === 'UTF-8') {
+            return $string;
+        }
+        if (!Horde_Util::extensionExists('iconv')) {
+            throw new Exception('Can not convert string to UTF-8. iconv extension not found.');
+        }
+        $converted = iconv($enc, "UTF-8", $string);
+        if ($converted === false) {
+            throw new Exception("iconv conversion from $enc to UTF-8 failed.");
+        }
+        return $converted;
     }
 }
