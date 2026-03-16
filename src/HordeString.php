effab6a90024189b881e0d627e50fac03d3be326
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-16 20:30:15 +0100

    fix(string): validate charset before mb_substr in PSR-4 version
    Apply charset validation fix from commit 63d0ea4 to src/HordeString.php equivalent.
    This prevents PHP errors when mb_substr is called with unsupporte charsets.
    
    The lib/Horde/String.php version was fixed in Feb 2025 by @tdannhauer but not applied to the PSR-4 version.

--- src/HordeString.php
+++ src/HordeString.php
@@ -339,11 +339,14 @@ class HordeString
 
         /* Try mbstring. */
         if (Util::extensionExists('mbstring')) {
-            $ret = @mb_substr($string, $start, $length, self::_mbstringCharset($charset));
+            $supported_encodings = mb_list_encodings();
+            if (in_array($charset, $supported_encodings)) {
+                $ret = @mb_substr($string, $start, $length, self::_mbstringCharset($charset));
 
-            /* mb_substr() returns empty string on failure. */
-            if (strlen($ret)) {
-                return $ret;
+                /* mb_substr() returns empty string on failure. */
+                if (strlen($ret)) {
+                    return $ret;
+                }
             }
             $error = true;
         }
